ff6d17ae909bc0d479d1c374b4772fed6133b58a
Beatrice Olivera <beatriceolivera1999@gmail.com>
2018-09-01 18:27:12 +0100

    fixed active/inactive detail on navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,26 +4,35 @@ import { triggerModalEvent } from "../components/modal.js";
 
 triggerModalEvent();
 
-const links = document.querySelector('links');
-const linksList = document.querySelectorAll('.links div');
+const settingsPage = document.getElementById('settings-page');
+const contactsPage = document.getElementById('contacts-page');
 
-const getSiblings = function (elem) {
+const getSiblings = (element) => {
   const siblings = [];
-  let sibling = elem.parentNode.firstChild;
-  const skipMe = elem;
+  let sibling = element.parentNode.firstChild;
+  const skipMe = element;
   for ( ; sibling; sibling = sibling.nextSibling )
-    if ( sibling.nodeType == 1 && sibling != elem )
+    if ( sibling.nodeType == 1 && sibling != element )
       siblings.push( sibling );
   return siblings;
 }
 
-linksList.forEach(link => {
-  link.addEventListener('click', event => {
-    // event.preventDefault()
-    link.classList.add('active')
-    const siblings = getSiblings(link)
-    siblings.forEach(sibling => {
-      sibling.classList.remove('active');
-    })
-  })
-})
+const removeActiveClass = (element) => {
+  const siblings = getSiblings(element);
+  siblings.forEach(sibling => {
+    sibling.classList.remove('active');
+  });
+}
+
+if (settingsPage) {
+  const settings = document.getElementById('settings')
+  settings.classList.add('active');
+  removeActiveClass(settings);
+}
+
+if (contactsPage) {
+  const contacts = document.getElementById('contacts')
+  contacts.classList.add('active');
+  removeActiveClass(contacts);
+}
+
